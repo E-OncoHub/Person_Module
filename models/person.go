6e0467ebf3dd6c714196a707e0dac99120869876
andreiserban123 <andyspeed2003@gmail.com>
2024-07-20 16:37:05 +0300

    final

--- models/person.go
+++ models/person.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"eoncohub.com/person_module/db"
+	"time"
+)
 
 type Person struct {
 	ID               int       `json:"id_person"`
@@ -10,4 +13,13 @@ type Person struct {
 	BornDate         time.Time `json:"born_date"`
 	IDAddress        int       `json:"id_address"`
 	IDVirtualAddress int       `json:"id_virtual_address"`
+}
+
+func GetPersonById(id int) (Person, error) {
+	var person Person
+	err := db.DB.QueryRow("SELECT id_person, f_name, l_name, cnp, born_date, id_address, id_virtual_address FROM PERSONS WHERE id_person = :1", id).Scan(&person.ID, &person.FirstName, &person.LastName, &person.CNP, &person.BornDate, &person.IDAddress, &person.IDVirtualAddress)
+	if err != nil {
+		return person, err
+	}
+	return person, nil
 }
