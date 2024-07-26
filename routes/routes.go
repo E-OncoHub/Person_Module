822a5b16b695abc0c4ff77edd82ae522deef6a9e
andreiserban123 <andyspeed2003@gmail.com>
2024-07-26 14:02:44 +0300

    clean up

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,13 @@ package routes
 import "github.com/labstack/echo/v4"
 
 func RegisterRoutes(server *echo.Echo) {
+	// Virtual Address routes
 	server.POST("/virtual_address", createVirtualAddress)
+
+	// Person routes
 	server.POST("/person/create", createPerson)
 	server.GET("/person/:id", getPerson)
 	server.PUT("/person/update/:id", updatePerson)
 	server.DELETE("/person/delete/:id", deletePerson)
+
 }
